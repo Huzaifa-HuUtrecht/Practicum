97e2e501c271275f7450271f1593cd54cf8f0fab
Huzaifa-HuUtrecht <huzaifa.balaksi@student.hu.nl>
2024-11-16 12:13:54 +0100

    practicum 2B

---
+++ project/src/practicum2B/Voetbalclub.java
@@ -0,0 +1,37 @@
+package practicum2B;
+
+public class Voetbalclub {
+
+    private String naam;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
+
+    public Voetbalclub(String naam) {
+        this.naam = naam;
+    }
+
+    public void verwerkResultaat(char ch) {
+
+        if (ch == 'w')
+            aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+            aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+            aantalVerloren = aantalVerloren + 1;
+    }
+
+    public int aantalPunten() {
+        return (aantalGewonnen * 3) + aantalGelijk;
+    }
+
+    public int aantalGespeeld() {
+        return aantalGelijk + aantalGewonnen + aantalVerloren;
+    }
+
+    @Override
+    public String toString() {
+        return naam + "   " + aantalGespeeld() + "   " + aantalGewonnen + "   " + aantalGelijk + "   " + aantalVerloren + "   " + aantalPunten();
+    }
+
+}
