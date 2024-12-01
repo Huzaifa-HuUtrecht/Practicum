f86b85b9fbffdbd9e70accb221df919f0f87e017
Huzaifa-HuUtrecht <huzaifa.balaksi@student.hu.nl>
2024-12-01 21:59:10 +0100

    Practicum 4A feedback

--- project/src/practicum4A/Huis.java
+++ project/src/practicum4A/Huis.java
@@ -5,18 +5,20 @@ public class Huis {
     private int bouwjaar;
     private Persoon huisbaas;
 
-    public Huis(String adres, int bouwjaar) {
-        this.adres = adres;
-        this.bouwjaar = bouwjaar;
+    public Huis(String adr, int bwjr) {
+        this.adres = adr;
+        this.bouwjaar = bwjr;
+    }
+
+    public void setHuisbaas(Persoon hb) {
+        this.huisbaas = hb;
     }
 
     public Persoon getHuisbaas() {
         return huisbaas;
     }
 
-    public void setHuisbaas(Persoon huisbaas) {
-        this.huisbaas = huisbaas;
-    }
+
 
     @Override
     public String toString() {
