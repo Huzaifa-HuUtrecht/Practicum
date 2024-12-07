6397bae964dfc1ef73253a735bdbedeb045861c2
Huzaifa-HuUtrecht <huzaifa.balaksi@student.hu.nl>
2024-12-07 16:29:43 +0100

    practicum 6A

--- practicum6/src/practicum6A/Game.java
+++ practicum6/src/practicum6A/Game.java
@@ -1,4 +1,46 @@
 package practicum6A;
 
+import java.time.Year;
+
 public class Game {
+    private String naam;
+    private int releaseJaar;
+    private double nieuwprijs;
+
+    public Game(String nm, int rJ, double nwpr) {
+        this.naam = nm;
+        this.releaseJaar = rJ;
+        this.nieuwprijs = nwpr;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public double huidigeWaarde() {
+        int huidigeJaar = Year.now().getValue();
+        int aantalJaar = huidigeJaar - releaseJaar;
+
+        if (aantalJaar < 0) {
+            aantalJaar = 0;
+        }
+        return this.nieuwprijs * Math.pow(0.7, aantalJaar);
+    }
+
+    public boolean equals(Object andereObject) {
+        if (this == andereObject) {
+            return true;
+        }
+        if (!(andereObject instanceof Game)) {
+            return false;
+        }
+        Game game = (Game) andereObject;
+
+        return this.naam.equals(game.naam) && this.releaseJaar == game.releaseJaar;
+    }
+
+    @Override
+    public String toString() {
+        return naam + ", uitgegeven in " + releaseJaar + "; nieuwprijs: €" + String.format("%.2f", nieuwprijs) + " nu voor: €" + String.format("%.2f", huidigeWaarde());
+    }
 }
--- practicum6/src/practicum6A/Persoon.java
+++ practicum6/src/practicum6A/Persoon.java
@@ -1,4 +1,64 @@
 package practicum6A;
 
+import java.util.ArrayList;
+
 public class Persoon {
+    private String naam;
+    private double budget;
+    private ArrayList<Game> mijnGames = new ArrayList<Game>();
+
+    public Persoon(String nm, double bud) {
+        this.naam = nm;
+        this.budget = bud;
+        this.mijnGames = new ArrayList<Game>();
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public boolean koop(Game g) {
+        if (!mijnGames.contains(g) && g.huidigeWaarde() <= budget) {
+            mijnGames.add(g);
+
+            budget -= g.huidigeWaarde();
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verkoop(Game g, Persoon koper) {
+        if (!mijnGames.contains(g)) {
+            return false;
+        }
+        if (koper.mijnGames.contains(g)) {
+            return false;
+        }
+
+        if (koper.budget < g.huidigeWaarde()) {
+
+            return false;
+        }
+        koper.mijnGames.add(g);
+        koper.budget -= g.huidigeWaarde();
+        mijnGames.remove(g);
+        budget += g.huidigeWaarde();
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append(naam + " heeft een budget van €" + String.format("%.2f", budget) + " en bezit de volgende games:");
+
+        if (mijnGames.isEmpty()) {
+        } else {
+            for (Game g : mijnGames) {
+                result.append("\n" + g.toString());
+            }
+        }
+
+        return result.toString();
+    }
 }
